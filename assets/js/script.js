3237465d6f996856ae6f127e9f268d1354b3f56a
Erik Everton Rodrigues <erik.webprogrammer@gmail.com>
2026-05-25 15:24:10 -0300

    feat(G-3): ajustes no header e footer da página

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
 const hamburguer = document.querySelector(".hamburguer-container");
-const navigation = document.querySelector(".navigation");
+const navigation = document.querySelector(".navigation-mob");
 
 hamburguer.addEventListener("click", () => {
   hamburguer.classList.toggle("active");
   navigation.classList.toggle("active");
+});
+
+const links = document.querySelectorAll('.nav-link');
+
+links.forEach(link => {
+  if (link.href == window.location.href) {
+    link.classList.add('active');
+  }
 });
