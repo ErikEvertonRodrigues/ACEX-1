56dbdf3de24a0be0fa21934ebd72a726248384e3
Erik Everton Rodrigues <erik.webprogrammer@gmail.com>
2026-05-30 18:07:13 -0300

    feat(G-4): modifica menu e footer e adiciona novas imagens

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,23 @@ links.forEach(link => {
     link.classList.add('active');
   }
 });
+
+document.addEventListener('click', (event) => {
+  const menuClicked = navigation.contains(event.target);
+  const hamburguerClicked = hamburguer.contains(event.target);
+
+  if (!menuClicked && !hamburguerClicked && navigation.classList.contains('active')) {
+    toggle();
+  }
+});
+
+window.addEventListener('scroll', () => {
+  if (navigation.classList.contains('active')) {
+    toggle();
+  }
+}, {passive: true});
+
+function toggle() {
+  hamburguer.classList.toggle("active");
+  navigation.classList.toggle("active");
+}
