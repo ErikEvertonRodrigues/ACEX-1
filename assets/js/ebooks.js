49c0321ac3abb8832c71ad241afdf9019ac637e4
EduX-10 <dududepaula2006@gmail.com>
2026-06-01 14:37:33 -0300

    feat(JSE1): Implementa javascript dos ebooks e melhorias na página
    * feat(JSE1, JSE2, JSE3, JSE4) Implementa o carregamento dos cards de ebooks e filtro dinâmico pelo campo de busca
    
    * feat(JSE7): adiciona categoria, concerta estilos, adiciona botão de carregar mais e aplica filtros
    
    * feat(JS9): melhorias na página de ebook
    
    * correcao
    
    ---------
    
    Co-authored-by: Dudds <duddscosta@gmail.com>
    Co-authored-by: Erik Everton Rodrigues <erik.webprogrammer@gmail.com>

---
+++ assets/js/ebooks.js
@@ -0,0 +1,84 @@
+let ebooks = [];
+
+async function carregarEbooks(){
+    try {
+        const resposta = await fetch('../assets/js/data/ebooks.json');
+        ebooks = await resposta.json();
+        gerarEbooks(ebooks.slice(0, 6));
+    }catch(erro) {
+        console.log("Erro ao carregar o JSON:", erro);
+    }
+}
+
+carregarEbooks();
+
+const filterForm = document.querySelector('.filters__form');
+const inputBusca = document.querySelector('#busca');
+const inputArea = document.querySelector('#area');
+
+function aplicarFiltros() {
+    
+    const termoBuscado = inputBusca.value.toLowerCase().trim();
+    const areaEscolhida = inputArea.value.toLowerCase(); 
+
+    const ebooksFiltrados = ebooks.filter(ebook => {
+
+        const categoriaEbook = (ebook.categoria || "").toLowerCase();
+        const tituloEbook = (ebook.titulo || "").toLowerCase();
+
+        console.log(`categoria: ${categoriaEbook}`);
+        console.log(`area: ${areaEscolhida}`);
+
+        const tituloOk = tituloEbook.includes(termoBuscado);
+        const areaOk = areaEscolhida === "" || categoriaEbook.includes(areaEscolhida);
+
+        console.log(tituloOk);
+
+        return tituloOk && areaOk;
+    });
+
+    gerarEbooks(ebooksFiltrados);
+
+    btnCarregarMais.style.display = 'none';
+}
+
+inputBusca.addEventListener('input', aplicarFiltros); 
+inputArea.addEventListener('change', aplicarFiltros); 
+
+filterForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+});
+
+function gerarCard(ebook){
+    return `
+        <div class="card">
+            <span>${ebook.categoria}</span>
+            <img class="card-image" src=${ebook.imagem} alt="">
+            <div class="card-info">
+                <h2>${ebook.titulo}</h2>
+                <p>${ebook.autor}</p>
+                <p>${ebook.descricao.substring(0, 120)}...</p>
+                <p><i class="fa-regular fa-file-lines"></i> ${ebook.paginas} páginas</p>
+                <a class="download" href=""><i class="fa-solid fa-download"></i> Baixar PDF</a>
+            </div>
+        </div>
+    `
+}
+
+function gerarEbooks(listEbooks){
+    const mainCards = document.querySelector('.content');
+    const dtqCards = document.querySelector('#cards_ebooks_destaques');
+
+    const htmlMainCards = listEbooks.map(ebook => gerarCard(ebook)).join('');
+    mainCards.innerHTML = htmlMainCards;
+
+    const htmlDtqCards = listEbooks.filter(ebook => ebook.destaque === true).map(ebook => gerarCard(ebook)).join('');
+    dtqCards.innerHTML = htmlDtqCards;
+}
+
+const btnCarregarMais = document.querySelector('.carregar_mais');
+
+btnCarregarMais.addEventListener('click', () => {
+    gerarEbooks(ebooks);
+    btnCarregarMais.style.display = 'none';
+});
