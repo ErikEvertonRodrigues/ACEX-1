52a01b5f2eed26220f8c0c28e9efc911f7033c71
Erik Everton Rodrigues <erik.webprogrammer@gmail.com>
2026-07-06 14:15:26 -0300

    feat(G-13): Adiciona lógica do modo escuro

---
+++ assets/js/data/themes.js
@@ -0,0 +1,15 @@
+export const themes = {
+    light: {
+        logo: "/assets/imgs/logo-light.svg",
+        icon: "<i class='fa-solid fa-star-and-crescent'></i>",
+        logoFooter: "/assets/imgs/somente_nome_claro.svg",
+        logoIfba: "/assets/imgs/logo-ifba.png",
+    },
+
+    dark: {
+        logo: "/assets/imgs/logo-dark.svg",
+        icon: "<i class='fa-solid fa-sun color-primary'></i>",
+        logoFooter: "/assets/imgs/somente_nome_escuro.svg",
+        logoIfba: "/assets/imgs/logo-ifba-branca.png",
+    }
+};
